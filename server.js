b956250cef7d7787194053dd9d0d3524a77985ff
Ghostbr400 <moyseisneto2020@gmail.com>
2025-05-14 22:00:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;echo "# test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ghostbr400/test.git
-git push -u origin main
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
